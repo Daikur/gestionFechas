6dc11561d16822ad50814d17157d8c706210acaa
Daikur <ortegasanchezdavid@hotmail.com>
2016-02-01 10:19:33 +0100

    v1

---
+++ src/jdk7/ejercicio7.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdk7;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class ejercicio7 {
+
+    public static void main(String[] args) {
+        Date hoy = new Date();
+        GregorianCalendar gr = new GregorianCalendar();
+
+// Ejercicio 7
+        //a
+        int dia = gr.get(GregorianCalendar.DAY_OF_MONTH);
+        int mes = gr.get(GregorianCalendar.DAY_OF_MONTH);
+        int anyo = gr.get(GregorianCalendar.YEAR);
+        int hora = gr.get(GregorianCalendar.HOUR);
+        int minutos = gr.get(GregorianCalendar.MINUTE);
+
+        String diasS[] = {"domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"};
+
+        String mesA[] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
+            "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+
+        System.out.println("Hoy es " + diasS[dia] + ", día " + dia + " de " + mesA[gr.get(Calendar.MONTH)] + " del año " + anyo + " y son las " + hora + ":" + minutos + ".");
+
+        //b   
+        DateFormat formato_2 = new SimpleDateFormat("dd-MM-yy");
+        String fechaL = formato_2.format(hoy);
+        System.out.println(fechaL);
+
+        //c
+        DateFormat formato_3 = new SimpleDateFormat("yyyy.MM.yy H:m:s:S");
+        fechaL = formato_3.format(hoy);
+        System.out.println(fechaL);
+
+        //d
+        DateFormat formato_4 = new SimpleDateFormat("EEE,MMM d,''yy");
+        fechaL = formato_4.format(hoy);
+        System.out.println(fechaL);
+
+        //e
+        DateFormat formato_5 = new SimpleDateFormat("K:mm a");
+        fechaL = formato_5.format(hoy);
+        System.out.println(fechaL);
+
+        //f
+        DateFormat formato_6 = new SimpleDateFormat("H:m:s:S");
+        fechaL = formato_6.format(hoy);
+        System.out.println(fechaL);
+
+        //6
+        DateFormat formato_7 = new SimpleDateFormat("yyyy.MMMM.dd H:m");
+        fechaL = formato_7.format(hoy);
+        System.out.println(fechaL);
+    }
+
+}
---
+++ src/jdk7/fechas.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdk7;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class fechas {
+
+    public static void main(String[] args) {
+        Date hoy = new Date();
+        System.out.println(hoy);
+
+        GregorianCalendar d1 = new GregorianCalendar(2016, 7, 30);
+        Date fecha = d1.getTime();
+        System.out.println(fecha);
+
+        GregorianCalendar d2 = new GregorianCalendar(2013, 11, 30, 14, 30, 14);
+        Date fecha_hora = d2.getTime();
+        System.out.println(fecha_hora);
+
+        GregorianCalendar d3 = new GregorianCalendar();
+        d3.setTime(hoy);
+        int anyo = d3.get(GregorianCalendar.YEAR);
+        System.out.println("año "+anyo);
+        int mes = d3.get(GregorianCalendar.MONTH);
+        System.out.println("mes "+mes);
+        int dia = d3.get(GregorianCalendar.DAY_OF_MONTH);
+        System.out.println("dia "+dia);
+        int dia_semana = d3.get(GregorianCalendar.DAY_OF_WEEK);
+        System.out.println(dia_semana);
+        new SimpleDateFormat("HH:mm:ss");
+        int hora = d3.get(GregorianCalendar.HOUR);
+        int min = d3.get(GregorianCalendar.MINUTE);
+        
+        System.out.println(hora+":"+min);
+
+        d3.add(GregorianCalendar.YEAR, 3);
+        Date d4 = d3.getTime();
+        System.out.println(d4);
+
+        DateFormat formatF = DateFormat.getDateInstance();
+        String formato_fecha = formatF.format(hoy);
+        System.out.println(formato_fecha);
+        //a
+        DateFormat formatF1 = DateFormat.getDateInstance(DateFormat.FULL);
+        String formato2_fecha = formatF1.format(hoy);
+        System.out.println("a: "+formato2_fecha);
+
+        Locale fEspañol = new Locale("es", "ES", "Traditional WIN");
+        DateFormat formatF3 = DateFormat.getDateInstance(DateFormat.FULL, fEspañol);
+        String formato3_fecha = formatF3.format(hoy);
+
+    }
+
+}
